a32122b96b495584412950fe1ee60450d8a78a1b
Christopher Rhodes <chrhodes@cox.net>
2020-12-28 09:51:25 -0800

    Initial Commit

---
+++ Pull-AllRepositories.ps1
@@ -0,0 +1,40 @@
+
+function delimitmsg($msg)
+{
+    $delimitS = "********** "
+    $delimitE = " **********"
+
+    Write-Host -ForegroundColor Red $delimitS ("{0,-30}" -f $msg) $delimitE
+}
+
+$RepoHome = "P:\git\chrhodes"
+
+$repos = @(
+    "Applications"
+    ,"AZDORestApiExplorer"
+    ,"CustomPoolAndSpa",
+    ,"Explore"
+    ,"EyeOnLife"
+    ,"GitTraining"
+    ,"GitTrainingContent"
+    ,"Office"
+    ,"SMARTS"
+    ,"VisualStudio"
+    ,"VNC"
+    ,"VNC Tracking System"
+    ,"VNCCodeCommandConsole"
+    ,"VNCLogViewer"
+    )
+
+    cd $RepoHome
+
+    foreach ($repo in $repos)
+    {
+        cd "$RepoHome\$repo"
+        delimitmsg "$RepoHome\$repo - git pull --progress -v --no-rebase ""origin"" master"
+        git.exe pull --progress -v --no-rebase "origin" master
+        
+        cd ..
+    }
+    
+    Read-Host -Prompt "Press Enter to Exit"
